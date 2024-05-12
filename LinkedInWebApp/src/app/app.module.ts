8e0dd16d4b5089c7ffda79daeca10749b2e4206f
damizer <andreas.papadopoulos36@outlook.com.gr>
2024-05-12 21:06:16 +0300

    changes to main page and assets

--- LinkedInWebApp/src/app/app.module.ts
+++ LinkedInWebApp/src/app/app.module.ts
@@ -1,3 +1,4 @@
+/* app.module.tss */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
