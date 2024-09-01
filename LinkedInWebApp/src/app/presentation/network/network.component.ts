282c30a8d963cc64bb0b43374a566f4d2474e293
damizer <andreas.papadopoulos36@outlook.com.gr>
2024-09-01 20:37:10 +0300

    arrangements on ui

--- LinkedInWebApp/src/app/presentation/network/network.component.ts
+++ LinkedInWebApp/src/app/presentation/network/network.component.ts
@@ -13,21 +13,22 @@ export class NetworkComponent implements OnInit {
   searchQuery: string = '';
 
   constructor(private userService: UserService, private router: Router) {}
-
+  selectedProfessional: any = null;
   ngOnInit() {
     this.loadConnectedProfessionals();
   }
 
   loadConnectedProfessionals() {
-    this.userService.getConnectedProfessionals().subscribe({
+    this.userService.getAllUsers().subscribe({
       next: (data) => {
-        this.connectedProfessionals = data;
+        this.connectedProfessionals = data; 
       },
       error: (err) => {
         console.error('Error loading connected professionals:', err);
       },
     });
   }
+  
 
   searchProfessionals() {
     if (this.searchQuery) {
@@ -43,7 +44,9 @@ export class NetworkComponent implements OnInit {
       this.searchResults = [];
     }
   }
-
+  viewProfile(professional: any) {
+    this.selectedProfessional = professional;
+  }
   startPrivateChat(professionalId: string) {
     this.router.navigate(['/discussions'], { queryParams: { id: professionalId } });
   }
--- LinkedInWebApp/src/app/presentation/welcome/welcome.component.ts
+++ LinkedInWebApp/src/app/presentation/welcome/welcome.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth-service/auth.service';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../../services/local-storage/local-storage.service';
 
 @Component({
   selector: 'app-welcome',
@@ -8,19 +10,48 @@ import { AuthService } from '../../services/auth-service/auth.service';
   styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent {
-  user = { email: '', password: '' };
+  loginForm!: FormGroup;
+  errorMessage: string | null = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private localStorageService: LocalStorageService,
+  ) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
 
   onLogin() {
-    console.log('Login form submitted', this.user);
-    this.authService.login(this.user.email, this.user.password).subscribe({
-      next: (response) => {
-        console.log('Login successful', response);
-      },
-      error: (error) => {
-        console.error('Login failed', error);
-      },
-    });
+    if (this.loginForm.valid) {
+      this.authService
+        .login(this.loginForm.value.email, this.loginForm.value.password)
+        .subscribe({
+          next: (response) => {
+            console.log('Login response', response);
+            const userLogedIn = this.localStorageService.returnUser();
+            console.log('User logged in', userLogedIn);
+            if (userLogedIn?.role === 'admin') {
+              this.router.navigate(['/admin']);
+            } else {
+              this.router.navigate(['/home']);
+            }
+          },
+          error: (error) => {
+            this.errorMessage = error.error.Message || 'User login failed';
+          },
+        });
+    } else {
+      console.log('Invalid form.');
+    }
   }
 }
