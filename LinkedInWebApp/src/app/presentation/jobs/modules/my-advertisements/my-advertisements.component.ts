cfff6323cb88bb6363fa143c1989f2f9342a6835
damizer <andreas.papadopoulos36@outlook.com.gr>
2024-09-26 17:15:50 +0300

    my-advertisements edits

--- LinkedInWebApp/src/app/presentation/jobs/modules/my-advertisements/my-advertisements.component.ts
+++ LinkedInWebApp/src/app/presentation/jobs/modules/my-advertisements/my-advertisements.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AdvertisementDto } from '../../models/advertisement.model';
+import { AdvertisementDto, AdvertisementRequest } from '../../models/advertisement.model';
 import { AdvertisementService } from '../../services/advertisement.service';
 import { GlobalConstantsService } from '../../../../services/global-constants.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-advertisements',
@@ -11,7 +12,9 @@ import { GlobalConstantsService } from '../../../../services/global-constants.se
 })
 export class MyAdvertisementsComponent implements OnInit {
   advertisements: AdvertisementDto[] = [];
-  selectedAdvertisement: AdvertisementDto | null = null; 
+  selectedAdvertisement: AdvertisementDto | null = null;
+  editForm: FormGroup; 
+  showEditForm: boolean = false; 
   jobTypes: any[] = [];
   workingLocations: any[] = [];
   profesionalBranches: any[] = [];
@@ -19,8 +22,18 @@ export class MyAdvertisementsComponent implements OnInit {
   constructor(
     private advertisementService: AdvertisementService,
     private router: Router,
-    private globalConstantsService: GlobalConstantsService
-  ) {}
+    private globalConstantsService: GlobalConstantsService,
+    private fb: FormBuilder
+  ) {
+    this.editForm = this.fb.group({
+      title: ['', Validators.required],
+      freeTxt: ['', Validators.required],
+      professionalBranche: ['', Validators.required],
+      jobType: ['', Validators.required],
+      workingLocation: ['', Validators.required],
+      status: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
     this.loadJobTypes();
@@ -77,7 +90,54 @@ export class MyAdvertisementsComponent implements OnInit {
     this.selectedAdvertisement = advertisement;
   }
 
-  editAdvertisement(id: number): void {
-    this.router.navigate(['/jobs/advertisements/update', id]);
+  openEditForm(advertisement: AdvertisementDto): void {
+    this.selectedAdvertisement = advertisement;
+    this.showEditForm = true; 
+
+    this.editForm.patchValue({
+      title: advertisement.title,
+      freeTxt: advertisement.freeTxt,
+      professionalBranche: advertisement.professionalBranche,
+      jobType: advertisement.jobType,
+      workingLocation: advertisement.workingLocation,
+      status: advertisement.status,
+    });
   }
+
+
+  closeEditForm(): void {
+    this.showEditForm = false;
+  }
+
+
+onSubmitEditForm(): void {
+  if (this.editForm.valid && this.selectedAdvertisement) {
+    const updatedAdvertisement: AdvertisementRequest = {
+      id: this.selectedAdvertisement.id,
+      title: this.editForm.value.title,
+      freeTxt: this.editForm.value.freeTxt,
+      status: Number(this.editForm.value.status), 
+      professionalBranche: Number(this.editForm.value.professionalBranche), 
+      jobType: Number(this.editForm.value.jobType), 
+      workingLocation: Number(this.editForm.value.workingLocation), 
+    };
+
+    // Call the update method in AdvertisementService
+    this.advertisementService.updateJob(updatedAdvertisement).subscribe({
+      next: (response) => {
+        this.loadAdvertisements();
+        this.closeEditForm();
+      },
+      error: (error) => {
+        console.error('Error updating advertisement', error);
+      },
+    });
+  }
+}
+loadAdvertisements(): void {
+  this.advertisementService.getMyAdvertisement().subscribe((advertisements) => {
+    this.advertisements = advertisements;
+  });
+}
+
 }
