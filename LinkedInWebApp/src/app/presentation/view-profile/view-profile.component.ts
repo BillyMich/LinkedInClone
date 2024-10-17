de07041593a7133da16f592a69d206c45435f3e4
damizer <andreas.papadopoulos36@outlook.com.gr>
2024-10-17 20:26:51 +0300

    adjustments on view-profile, fixes on profile

--- LinkedInWebApp/src/app/presentation/view-profile/view-profile.component.ts
+++ LinkedInWebApp/src/app/presentation/view-profile/view-profile.component.ts
@@ -1,8 +1,10 @@
+// view-profile.component.ts
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { SettingsService } from '../../services/settings.service';
 import { NewContactRequestDto } from '../network/models/network.model';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-view-profile',
@@ -12,6 +14,8 @@ import { NewContactRequestDto } from '../network/models/network.model';
 export class ViewProfileComponent implements OnInit {
   user: any;
   profilePictureUrl: string | ArrayBuffer | null = null;
+  experience: any[] = [];
+  education: any[] = [];
 
   constructor(
     private userService: UserService,
@@ -27,6 +31,7 @@ export class ViewProfileComponent implements OnInit {
         next: (data) => {
           this.user = data;
           this.loadProfilePicture(userId);
+          this.loadExperienceAndEducation(Number(userId));
         },
         error: (err) => {
           console.error('Error loading user:', err);
@@ -40,6 +45,29 @@ export class ViewProfileComponent implements OnInit {
       Number(userId)
     );
   }
+
+  loadExperienceAndEducation(userId: number) {
+    // Load public experiences
+    this.userService.getUserExperience(userId).subscribe({
+      next: (experiences) => {
+        this.experience = experiences.filter((exp: any) => exp.isPublic);
+      },
+      error: (error) => {
+        console.error('Error fetching experiences:', error);
+      },
+    });
+
+    // Load public educations
+    this.userService.getUserEducation(userId).subscribe({
+      next: (educations) => {
+        this.education = educations.filter((edu: any) => edu.isPublic);
+      },
+      error: (error) => {
+        console.error('Error fetching education:', error);
+      },
+    });
+  }
+
   startPrivateChat(professionalId: string) {
     this.router.navigate(['/discussions'], {
       queryParams: { id: professionalId },
@@ -59,7 +87,21 @@ export class ViewProfileComponent implements OnInit {
         },
       });
   }
+
   onImageError(event: any) {
     event.target.src = '../../../assets/user-profile-picture.jpg';
   }
+
+  formatDuration(startDateStr: string, endDateStr?: string): string {
+    const startDate = new Date(startDateStr);
+    const endDate = endDateStr ? new Date(endDateStr) : new Date();
+
+    const startMonthYear = formatDate(startDate, 'LLLL yyyy', 'el');
+    const endMonthYear = endDateStr
+      ? formatDate(endDate, 'LLLL yyyy', 'el')
+      : 'Σήμερα';
+
+    return `${startMonthYear} - ${endMonthYear}`;
+  }
 }
+
